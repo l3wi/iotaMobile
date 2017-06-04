84ead6c85fa95e6c12a8654b8aaed626a09abd53
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-04 23:08:57 +1000

    Feature/Add new routes

--- app/entry.js
+++ app/entry.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import styled from "styled-components/native";
 import { AppRegistry, StyleSheet, Text, View } from "react-native";
-import { StackNavigator } from "react-navigation";
+import { StackNavigator, DrawerNavigator } from "react-navigation";
 
 import InitialScreen from "./routes/initial";
 import HomeScreen from "./routes/home";
 
-export default (SimpleApp = StackNavigator({
-  Home: { screen: HomeScreen },
-  Initial: { screen: InitialScreen }
-}));
+import Iota from "./libs/iota";
+
+const MainScreenNavigator = DrawerNavigator({
+  Home: { screen: HomeScreen }
+});
+
+export default (SimpleApp = StackNavigator(
+  {
+    Initial: { screen: InitialScreen },
+    Main: { screen: MainScreenNavigator }
+  },
+  { headerMode: "screen" }
+));
