bc83756b6553e49150aa959ee2247eee8f266fa9
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-05 08:43:36 +1000

    Feaure/Style home route

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,17 +2,27 @@ import React, { Component } from "react";
 import styled from "styled-components/native";
 import { AppRegistry, StyleSheet, Text, View } from "react-native";
 
+import Balance from "../components/balance";
+
 export default class HomeScreen extends Component {
   static navigationOptions = {
     header: null
   };
   render() {
     return (
-      <View>
+      <Wrapper>
+        <Balance />
         <Text>
           Home Page
         </Text>
-      </View>
+      </Wrapper>
     );
   }
 }
+const Wrapper = styled.View`
+    height: 100%;
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    background: #004f71;
+`;
