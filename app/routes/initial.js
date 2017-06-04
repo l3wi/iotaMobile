89625cdcade817c55230706f20201e3697cbe457
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-04 23:08:23 +1000

    Feature/Setup initial routes in entry

--- app/routes/initial.js
+++ app/routes/initial.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
 import styled from "styled-components/native";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import LoginForm from "../components/login";
 
 export default class InitialScreen extends Component {
+  static navigationOptions = {
+    header: null
+  };
+
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Text>
-          Initial Page
-        </Text>
-      </View>
+      <Wrapper>
+        <LoginForm {...this.props} />
+        <Row>
+
+          <AppText>
+            ver 0.0.1
+          </AppText>
+        </Row>
+
+      </Wrapper>
     );
   }
 }
+const Wrapper = styled.View`
+    height: 100%;
+    display:flex;
+    justify-content: space-between;
+    background: #004f71;
+    padding: 30% 10px 20%;
+`;
+
+const Row = styled.View`
+    display: flex;
+    flex-direction: row;   
+    justify-content: center;
+    align-items: center;
+`;
+const AppText = styled.Text`
+    color: white;
+    font-family: courier;
+`;
