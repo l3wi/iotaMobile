94b0c1b36618ba0d7fd9db7ef07ba237e91d35c8
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-06 00:49:55 +1000

    Feature/Working Login to BOX

--- app/components/login/index.js
+++ app/components/login/index.js
@@ -2,39 +2,51 @@ import React from "react";
 import styled from "styled-components/native";
 import { AppRegistry, StyleSheet, Text, View, TextInput } from "react-native";
 import Iota, { Valid } from "../../libs/iota";
+import { OpenBox, SaveBox, DeleteBox } from "../../libs/crypto";
 
 export default class LoginForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: "" };
+    this.state = { pass: "" };
   }
 
-  getAccount(seed) {
-    Iota.node();
-    Iota.getAccount(seed);
-    this.props.navigation.navigate("Home");
-  }
+  getAccount = async (seed, password) => {
+    const clearSeed = await OpenBox("seed", password);
+    this.setState({ pass: "" });
+    if (!clearSeed) return alert("Incorrect Password");
+    Iota.getAccount(clearSeed).then(box => {
+      console.log(box);
+    });
+  };
   render() {
     return (
       <Col>
-        <Row>
-          <BottomBorder>
-            <TInput
-              value={this.state.text}
-              placeholder={"Enter Password"}
-              autoCorrect={false}
-              secureTextEntry={true}
-              placeholderTextColor={"white"}
-              selectTextOnFocus={true}
-              onChangeText={text => this.setState({ text })}
-              onFocus={() => this.setState({ text: "" })}
-            />
-          </BottomBorder>
+        <EmptyCol>
+          <Row>
+            <BottomBorder>
+              <TInput
+                value={this.state.pass}
+                placeholder={"Enter Password"}
+                autoCorrect={false}
+                secureTextEntry={true}
+                placeholderTextColor={"white"}
+                selectTextOnFocus={true}
+                onChangeText={pass => this.setState({ pass })}
+              />
+            </BottomBorder>
+          </Row>
+          <Row>
+            <Button
+              onPress={() => this.getAccount(this.state.pass, this.state.pass)}
+            >
+              <AppText>Login</AppText>
+            </Button>
+          </Row>
+        </EmptyCol>
 
-          <Button onPress={() => this.getAccount(this.state.text, this.props)}>
-            <AppText>login</AppText>
-          </Button>
-        </Row>
+        <Button onPress={() => DeleteBox("seed")}>
+          <AppText>Load New Wallet</AppText>
+        </Button>
       </Col>
     );
   }
@@ -49,18 +61,24 @@ const Row = styled.View`
 `;
 const Col = styled.View`
     display: flex;
+    height:80%;
+    width:80%;
     flex-direction: column;   
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
 `;
-
+const EmptyCol = styled.View`
+    display: flex;
+    flex-direction: column;
+`;
 const BottomBorder = styled.View`
-    width: 60%;
+    width: 100%;
     border-bottom-width: 3px;
-        border-bottom-color: white;
+      border-bottom-color: white;
 `;
 const TInput = styled.TextInput`
     height: 40px;
+    width: 100%;
     color: white;
     font-family: courier;
     text-align: center;
