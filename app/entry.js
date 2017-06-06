9ce65e8c1f9174f3d29fa595df38e89647882fbd
Lewis Freiberg <lewis.freiberg@cheproximity.com.au>
2017-06-06 16:19:40 +1000

    Fix/Update Entry

--- app/entry.js
+++ app/entry.js
@@ -5,17 +5,28 @@ import { StackNavigator, DrawerNavigator } from "react-navigation";
 
 import InitialScreen from "./routes/initial";
 import HomeScreen from "./routes/home";
+import SendScreen from "./routes/send";
 
 import Iota from "./libs/iota";
 
-const MainScreenNavigator = DrawerNavigator({
-  Home: { screen: HomeScreen }
-});
+const MainScreenNavigator = DrawerNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Send: { screen: SendScreen },
+    Recieve: { screen: SendScreen },
+    Settings: { screen: SendScreen }
+  },
+  {
+    navigationOptions: {
+      headerMode: "none"
+    }
+  }
+);
 
 export default (SimpleApp = StackNavigator(
   {
     Initial: { screen: InitialScreen },
     Main: { screen: MainScreenNavigator }
   },
-  { headerMode: "screen" }
+  { headerMode: "none" }
 ));
