ac2ad38271e2b62c85e2624f7ceef641c94366e9
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-10 16:25:32 +1000

    Feature/Simple modal for better inspection in transfers

--- app/components/transactions/index.js
+++ app/components/transactions/index.js
@@ -6,7 +6,9 @@ import {
   TextInput,
   Text,
   Dimensions,
-  TouchableOpacity
+  TouchableOpacity,
+  Modal,
+  Image
 } from "react-native";
 import Iota, { Valid } from "../../libs/iota";
 import format from "date-fns/format";
@@ -16,7 +18,8 @@ export default class LoginForm extends React.Component {
     super(props);
     this.state = {
       account: false,
-      loading: true
+      loading: true,
+      modalVisible: false
     };
   }
   componentWillReceiveProps(props) {
@@ -27,42 +30,117 @@ export default class LoginForm extends React.Component {
     this.setState({ account: this.props.account, loading: false });
   }
 
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
   render() {
     var { account, loading } = this.state;
     return (
-      <FlatList
-        data={
-          !account
-            ? []
-            : account.transfers.sort((a, b) => b[0].timestamp - a[0].timestamp)
-        }
-        keyExtractor={(item, index) => index}
-        renderItem={({ item, index }) => (
-          <Item key={index} width={width}>
-            <Row>
-              <Header {...item[0]}>{item[0].value} <Unit>Gi</Unit></Header>
-              <Text>
-                {item[0].persistence ? "Confirmed" : "Pending"}
-              </Text>
-            </Row>
-            <Row>
-              <Text>No Message</Text>
-              <Text>
-                {getDate(item[0].timestamp)}
-              </Text>
-            </Row>
-          </Item>
-        )}
-      />
+      <View>
+        <FlatList
+          data={
+            !account
+              ? []
+              : account.transfers.sort(
+                  (a, b) => b[0].timestamp - a[0].timestamp
+                )
+          }
+          keyExtractor={(item, index) => index}
+          renderItem={({ item, index }) => (
+            <Item
+              key={index}
+              width={width}
+              onPress={() => this.setModalVisible(!this.state.modalVisible)}
+            >
+              <Row>
+                <Header {...item[0]}>{item[0].value} <Unit>Gi</Unit></Header>
+                <Text>
+                  {item[0].persistence ? "Confirmed" : "Pending"}
+                </Text>
+              </Row>
+              <Row>
+                <Text>No Message</Text>
+                <Text>
+                  {getDate(item[0].timestamp)}
+                </Text>
+              </Row>
+            </Item>
+          )}
+        />
+        <Modal
+          animationType={"fade"}
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert("Modal has been closed.");
+          }}
+        >
+
+          <ModalBack>
+            <Close
+              onPress={() => {
+                this.setModalVisible(!this.state.modalVisible);
+              }}
+            >
+              <Image
+                source={require("../../assets/close.png")}
+                style={{ height: 30, width: 30 }}
+              />
+            </Close>
+            <ModalBody>
+              <Text>Hello World!</Text>
+
+              <Button
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}
+              >
+                <Text>Hide Modal</Text>
+              </Button>
+            </ModalBody>
+          </ModalBack>
+        </Modal>
+
+      </View>
     );
   }
 }
 const { height, width } = Dimensions.get("window");
 
+const ModalBody = styled.View`
+  padding: 20px;
+  width: 70%;
+  background: white;
+`;
+
+const Close = styled.TouchableOpacity`
+    position: absolute;
+    top: 40px;
+    right: 40px;
+`;
+
+const Button = styled.TouchableOpacity`
+    flex-direction: row;
+    justify-content: center;
+    padding: 10px;
+    margin: 10px 0;
+    background-color: rgba(0,0,0,.3);
+`;
+
+const ModalBack = styled.View`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.70);
+`;
+
 const getDate = m => {
   return format(parse(m * 1000), "HH:mm - MM/DD/YYYY");
 };
-const Item = styled.View`
+const Item = styled.TouchableOpacity`
     width: ${props => props.width + "px"};
     padding: 20px 50px;
     margin-bottom: 5px;
