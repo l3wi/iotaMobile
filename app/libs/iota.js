c2dcbed3cf28eb80e6826927dadc035da620cdd7
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-17 17:02:36 +1000

    Enable changing remote

--- app/libs/iota.js
+++ app/libs/iota.js
@@ -1,8 +1,43 @@
 import IOTA from "iota.lib.js";
+import { AsyncStorage } from "react-native";
 
-export const iota = new IOTA({
+export var iota = new IOTA({
   provider: "http://node.iotawallet.info:14265/"
 });
+
+(async () => {
+  console.log("Initialising IOTA lib");
+  try {
+    await AsyncStorage.getItem("node", (err, result) => {
+      if (result) {
+        console.log(result);
+        iota = new IOTA({
+          provider: JSON.parse(result)
+        });
+      } else {
+        console.log("Using default node");
+        iota = new IOTA({
+          provider: "http://node.iotawallet.info:14265/"
+        });
+      }
+    });
+  } catch (error) {
+    // Error saving data
+  }
+})();
+
+export const changeRemoteNode = async url => {
+  console.log("Setting remote node to: " + url);
+  iota = new IOTA({
+    provider: url
+  });
+  try {
+    await AsyncStorage.setItem("node", JSON.stringify(url));
+  } catch (error) {
+    // Error saving data
+  }
+  alert("Node Changed");
+};
 
 export default class iotaWrapper {
   static node = () => {
