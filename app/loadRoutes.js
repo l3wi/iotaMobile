42f650dc6cab80b5671ca503990fd79e400f29e5
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-21 08:56:33 +1000

    Add routes in

--- app/loadRoutes.js
+++ app/loadRoutes.js
@@ -1,18 +1,19 @@
 import { Navigation } from "react-native-navigation";
-
-import TransactionScreen from "./routes/transactions";
+import { Provider } from "react-redux";
+// Screens
+import Transact from "./routes/transactions";
 import SendScreen from "./routes/send";
 import RecieveScreen from "./routes/recieve";
-import SettingsScreen from "./routes/settings";
+import Settings from "./routes/settings";
 import InitialScreen from "./routes/initial";
 import MenuScreen from "./routes/menu";
 
 // register all screens of the app (including internal ones)
-export function registerScreens() {
-  Navigation.registerComponent("auth", () => InitialScreen);
-  Navigation.registerComponent("menu", () => MenuScreen);
-  Navigation.registerComponent("transactions", () => TransactionScreen);
-  Navigation.registerComponent("receive", () => RecieveScreen);
-  Navigation.registerComponent("send", () => SendScreen);
-  Navigation.registerComponent("settings", () => SettingsScreen);
+export function registerScreens(store, Provider) {
+  Navigation.registerComponent("auth", () => InitialScreen, store, Provider);
+  Navigation.registerComponent("menu", () => MenuScreen, store, Provider);
+  Navigation.registerComponent("transactions", () => Transact, store, Provider);
+  Navigation.registerComponent("receive", () => RecieveScreen, store, Provider);
+  Navigation.registerComponent("send", () => SendScreen, store, Provider);
+  Navigation.registerComponent("settings", () => Settings, store, Provider);
 }
