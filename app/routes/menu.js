7a6aec8efd902c2346491ce89efa406ad08d59f8
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-21 08:56:41 +1000

    Create menu screen

--- app/routes/menu.js
+++ app/routes/menu.js
@@ -2,23 +2,12 @@ import React, { Component } from "react";
 import styled from "styled-components/native";
 import { AppRegistry, StyleSheet, Text, View, ScrollView } from "react-native";
 
-import Balance from "../components/balance";
-import Transactions from "../components/transactions";
-
 export default class HomeScreen extends Component {
   static navigationOptions = {};
   render() {
-    console.log(this.props);
-    var { account, node, loading } = this.props.screenProps.state;
     return (
       <Wrapper>
-        <Balance
-          account={account}
-          loading={loading}
-          node={node}
-          {...this.props}
-        />
-        <Transactions account={account} loading={loading} {...this.props} />
+        <Text>Hey</Text>
       </Wrapper>
     );
   }
