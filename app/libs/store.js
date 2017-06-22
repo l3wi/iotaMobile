bd350b0bb63b9b3d752677902be6adb97d2da002
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-23 05:28:21 +1000

    Add persistance to the store and wrap in ascyn

--- app/libs/store.js
+++ app/libs/store.js
@@ -3,22 +3,36 @@ import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 // Middleware
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
+
+import { AsyncStorage } from "react-native"; // we need to import AsyncStorage to use as a storage enging
+import { persistStore, autoRehydrate } from "redux-persist"; // add new import
 // Reducers
-import reducer from "../reducers";
-import initialState from "./initialState";
+import reducers from "../reducers";
+
 const loggerMiddleware = createLogger({
   predicate: (getState, action) => __DEV__
 });
-console.log(initialState);
 
-function configureStore(initialState) {
-  const enhancer = compose(
-    applyMiddleware(
-      thunkMiddleware, // lets us dispatch() functions
-      loggerMiddleware
-    )
-  );
-  return createStore(reducer, initialState, enhancer);
-}
+const initialState = {
+  iota: {
+    loading: false,
+    account: {},
+    node: {},
+    nodeUrl: "http://node.iotawallet.info:14265",
+    rememberMe: 60000
+  },
+  crypto: {
+    box: false
+  }
+};
 
-export const store = configureStore({});
+const middleWare = [thunkMiddleware, loggerMiddleware];
+
+const createStoreWithMiddleware = applyMiddleware(...middleWare)(createStore);
+
+export const configureStore = initialState => {
+  return new Promise((resolve, reject) => {
+    const store = autoRehydrate()(createStoreWithMiddleware)(reducers);
+    persistStore(store, { storage: AsyncStorage }, () => resolve(store));
+  });
+};
