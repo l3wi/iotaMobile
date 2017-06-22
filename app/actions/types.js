a31d3f317658fcabb70e4c51f45b471c5b45f46f
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-23 05:25:44 +1000

    Add new functs in

--- app/actions/types.js
+++ app/actions/types.js
@@ -2,15 +2,17 @@
 export const SET_ACCOUNT = "SET_ACCOUNT";
 export const SET_ADDRESS = "SET_ADDRESS";
 export const SET_NODE = "SET_NODE";
+export const SET_REMOTE = "SET_REMOTE";
 export const SEND = "SEND";
 export const REPLAY = "REPLAY";
 export const SET_PWD = "SET_PWD";
 
 // UTILS
 export const LOADING = "LOADING";
+export const HYDRATE = "HYDRATE";
 
 // Crypto
-export const BOX_FLAG = 'BOX_FLAG'
+export const BOX_FLAG = "BOX_FLAG";
 // export const RETRIEVE_BOX = 'RETRIEVE_BOX'
 // export const DELETE_BOX = 'DELETE_BOX'
 // export const OPEN_BOX = 'OPEN_BOX'
