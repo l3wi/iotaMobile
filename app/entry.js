3cccb36be0e7523206c6e16a1bc141e6418c5c9e
Lewis Freiberg <lewis.freiberg@cheproximity.com.au>
2017-06-22 16:49:05 +1000

    Fix?Clean entry

--- app/entry.js
+++ app/entry.js
@@ -1,40 +1,20 @@
-import { Navigation } from "react-native-navigation";
-import { Provider } from "react-redux";
+import { Navigation } from 'react-native-navigation'
+import { Provider } from 'react-redux'
 
-import { registerScreens } from "./loadRoutes";
-import { store } from "./store";
+import { registerScreens } from './loadRoutes'
+import { store } from './store'
 // screen related book keeping
-registerScreens(store, Provider);
+registerScreens(store, Provider)
 
 Navigation.startSingleScreenApp({
   screen: {
-    screen: "auth", // unique ID registered with Navigation.registerScreen
-    navigatorStyle: { navBarHidden: true }, // override the navigator style for the screen, see "Styling the navigator" below (optional)
-    navigatorButtons: {} // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
+    screen: 'auth',
+    navigatorStyle: { navBarHidden: true }
   },
   drawer: {
-    // optional, add this if you want a side menu drawer in your app
     left: {
-      // optional, define if you want a drawer from the left
-      screen: "menu", // unique ID registered with Navigation.registerScreen
-      passProps: {} // simple serializable object that will pass as props to all top screens (optional)
+      screen: 'menu'
     },
-    disableOpenGesture: false // optional, can the drawer be opened with a swipe instead of button
-  },
-  passProps: {} // simple serializable object that will pass as props to all top screens (optional)
-});
-// export default class Entry extends Component {
-//   render() {
-//     return (
-//       <Wrapper>
-//         <StatusBar backgroundColor="blue" barStyle="light-content" />
-//         <SimpleApp />
-//       </Wrapper>
-//     );
-//   }
-// }
-
-// const Wrapper = styled.View`
-//     height: 100%;
-//     width:100%;
-// `;
+    disableOpenGesture: false
+  }
+})
