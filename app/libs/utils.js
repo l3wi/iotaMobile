bcacf3a45baa2ed68b6113310d88642f0b7991aa
Lewis Freiberg <lewis@twopeaks.com.au>
2017-06-26 06:33:38 +1000

    Santise the number converter

--- app/libs/utils.js
+++ app/libs/utils.js
@@ -1,90 +1,92 @@
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
+import format from "date-fns/format";
+import parse from "date-fns/parse";
 
 export const getDate = m => {
-  return format(parse(m * 1000), 'HH:mm A - DD/MM')
-}
+  return format(parse(m * 1000), "HH:mm A - DD/MM");
+};
 
 // Partially taken from the IOTA Wallet
 export const formatAmount = (amount, ternary) => {
-  var units = 'i'
-  var negative = ''
+  var units = "i";
+  var negative = "";
   if (amount < 0) {
-    const amount = Math.abs(amount)
-    var negative = '-'
+    const amount = Math.abs(amount);
+    var negative = "-";
   }
-  var sanitisedAmount = 0
+  var sanitisedAmount = 0;
   if (amount >= 1000000000000000) {
-    units = 'Pi'
-    sanitisedAmount = sigFigs(reducer(amount, units), 4)
+    units = "Pi";
+    sanitisedAmount = sigFigs(reducer(amount, units), 4);
   } else if (amount >= 1000000000000) {
-    units = 'Ti'
-    sanitisedAmount = sigFigs(reducer(amount, units), 4)
+    units = "Ti";
+    sanitisedAmount = sigFigs(reducer(amount, units), 4);
   } else if (amount >= 1000000000) {
-    units = 'Gi'
-    sanitisedAmount = sigFigs(reducer(amount, units), 4)
+    units = "Gi";
+    sanitisedAmount = sigFigs(reducer(amount, units), 4);
   } else if (amount >= 1000000) {
-    units = 'Mi'
-    sanitisedAmount = sigFigs(reducer(amount, units), 4)
+    units = "Mi";
+    sanitisedAmount = sigFigs(reducer(amount, units), 4);
   } else {
-    units = 'i'
-    sanitisedAmount = amount
+    units = "i";
+    sanitisedAmount = amount;
   }
 
-  if (ternary === 'bal') {
-    return negative + sanitisedAmount.toString()
-  } else if (ternary === 'unit') {
-    return units
+  if (sanitisedAmount.isNaN) {
+    return "-";
+  } else if (ternary === "bal") {
+    return negative + sanitisedAmount.toString();
+  } else if (ternary === "unit") {
+    return units;
   } else {
-    return negative + sanitisedAmount.toString() + ' ' + units
+    return negative + sanitisedAmount.toString() + " " + units;
   }
-}
+};
 
 const sigFigs = (n, sig) => {
-  var mult = Math.pow(10, sig - Math.floor(Math.log(n) / Math.LN10) - 1)
-  return Math.round(n * mult) / mult
-}
+  var mult = Math.pow(10, sig - Math.floor(Math.log(n) / Math.LN10) - 1);
+  return Math.round(n * mult) / mult;
+};
 
 export const converter = (amount, unit) => {
   switch (unit) {
-    case 'Ki':
-      return amount * Math.pow(10, 3)
-      break
-    case 'Mi':
-      return amount * Math.pow(10, 6)
-      break
-    case 'Gi':
-      return amount * Math.pow(10, 9)
-      break
-    case 'Ti':
-      return amount * Math.pow(10, 12)
-      break
-    case 'Pi':
-      return amount * Math.pow(10, 15)
-      break
+    case "Ki":
+      return amount * Math.pow(10, 3);
+      break;
+    case "Mi":
+      return amount * Math.pow(10, 6);
+      break;
+    case "Gi":
+      return amount * Math.pow(10, 9);
+      break;
+    case "Ti":
+      return amount * Math.pow(10, 12);
+      break;
+    case "Pi":
+      return amount * Math.pow(10, 15);
+      break;
     default:
-      return amount
+      return amount;
   }
-}
+};
 
 export const reducer = (amount, unit) => {
   switch (unit) {
-    case 'Ki':
-      return amount / Math.pow(10, 3)
-      break
-    case 'Mi':
-      return amount / Math.pow(10, 6)
-      break
-    case 'Gi':
-      return amount / Math.pow(10, 9)
-      break
-    case 'Ti':
-      return amount / Math.pow(10, 12)
-      break
-    case 'Pi':
-      return amount / Math.pow(10, 15)
-      break
+    case "Ki":
+      return amount / Math.pow(10, 3);
+      break;
+    case "Mi":
+      return amount / Math.pow(10, 6);
+      break;
+    case "Gi":
+      return amount / Math.pow(10, 9);
+      break;
+    case "Ti":
+      return amount / Math.pow(10, 12);
+      break;
+    case "Pi":
+      return amount / Math.pow(10, 15);
+      break;
     default:
-      return amount
+      return amount;
   }
-}
+};
