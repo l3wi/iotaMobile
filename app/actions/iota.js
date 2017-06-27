592b73c7309aded76e7f2d14a08dc9e7437f9276
Lewis Freiberg <lewis.freiberg@cheproximity.com.au>
2017-06-27 15:02:59 +1000

    Handle port error

--- app/actions/iota.js
+++ app/actions/iota.js
@@ -35,7 +35,7 @@ export const changeNode = (remote, user) => {
 export const getNode = pwd => {
   return async (dispatch, getState) => {
     dispatch(startLoading('Connecting to Node'))
-    await iota.api.getNodeInfo(function(error, success) {
+    await iota.api.getNodeInfo(function (error, success) {
       if (error) {
         Alert.alert(error)
         return dispatch(finishLoading())
@@ -50,7 +50,7 @@ export const getNode = pwd => {
 export const getAccount = (pwd, navigator) => {
   return async (dispatch, getState) => {
     dispatch(startLoading('Getting your Wallet'))
-    await iota.api.getAccountData(await OpenBox('seed', pwd), function(
+    await iota.api.getAccountData(await OpenBox('seed', pwd), function (
       error,
       success
     ) {
@@ -76,11 +76,18 @@ export const getTransfers = (addresses, navigator) => {
       inputs: []
     }
     dispatch(startLoading('Updating transactions'))
-    iota.api._bundlesFromAddresses(addresses, true, function(error, bundles) {
+    iota.api._bundlesFromAddresses(addresses, true, function (error, bundles) {
       data.transfers = bundles
-      iota.api.getBalances(addresses, 100, function(error, balances) {
-        balances.balances.forEach(function(balance, index) {
-          var balance = parseInt(balance)
+      console.log(addresses)
+      iota.api.getBalances(addresses, 100, function (error, balances) {
+        console.log(balances)
+        if (!balances) {
+          Alert.alert('Error', 'Could not get bundles')
+          dispatch(finishLoading())
+          return
+        }
+        balances.balances.forEach(function (balance, index) {
+          var balance = parseInt(balance, 10)
           data.balance += balance
 
           // Uncomment if you want inouts
@@ -103,7 +110,7 @@ export const getTransfers = (addresses, navigator) => {
 export const newAddress = pwd => {
   return async (dispatch, getState) => {
     dispatch(startLoading('Generating new Address'))
-    iota.api.getNewAddress(await OpenBox('seed', pwd), function(
+    iota.api.getNewAddress(await OpenBox('seed', pwd), function (
       error,
       success
     ) {
@@ -126,7 +133,7 @@ export const sendTransaction = (pwd, depth, minMag, transfers) => {
       depth,
       minMag,
       transfers,
-      function(error, success) {
+      function (error, success) {
         dispatch(finishLoading())
         if (error) {
           alert(error)
@@ -138,7 +145,7 @@ export const sendTransaction = (pwd, depth, minMag, transfers) => {
 
 export const reattachTransaction = (depth, minMag, hash) => {
   return async (dispatch, getState) => {
-    iota.api.replayBundle(depth, minMag, hash, function(error, success) {
+    iota.api.replayBundle(depth, minMag, hash, function (error, success) {
       dispatch(finishLoading())
       if (error) {
         alert(error)
@@ -165,61 +172,61 @@ export const resetAsync = navigator => {
   }
 }
 
-export function clearIota(data) {
+export function clearIota (data) {
   return {
     type: types.CLEAR_IOTA,
     data
   }
 }
 
-export function hydrate(data) {
+export function hydrate (data) {
   return {
     type: types.HYDRATE,
     data
   }
 }
 
-export function startLoading(data) {
+export function startLoading (data) {
   return {
     type: types.LOADING,
     data
   }
 }
 
-export function finishLoading() {
+export function finishLoading () {
   return {
     type: types.LOADING
   }
 }
 
-export function saveRemoteNode(node) {
+export function saveRemoteNode (node) {
   return {
     type: types.SET_REMOTE,
     node
   }
 }
 
-export function setNodeInfo(node) {
+export function setNodeInfo (node) {
   return {
     type: types.SET_NODE,
     node
   }
 }
 
-export function updateState(data) {
+export function updateState (data) {
   return {
     type: types.UPDATE_TRANSFERS,
     data
   }
 }
-export function setAccount(account) {
+export function setAccount (account) {
   return {
     type: types.SET_ACCOUNT,
     account
   }
 }
 
-export function setAddress(address) {
+export function setAddress (address) {
   return {
     type: types.SET_ADDRESS,
     address
