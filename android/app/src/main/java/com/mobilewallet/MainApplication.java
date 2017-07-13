7622e3284fdf3a734aa8d34d75642c818c5f02bb
Lewis Freiberg <lewi@2pk.in>
2017-07-13 22:37:57 +1000

    Enable builds on Android

--- android/app/src/main/java/com/mobilewallet/MainApplication.java
+++ android/app/src/main/java/com/mobilewallet/MainApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.reactnativenavigation.NavigationReactPackage;
 import com.corbt.keepawake.KCKeepAwakePackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.oblador.keychain.KeychainPackage;
@@ -14,39 +13,33 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.reactnativenavigation.NavigationApplication;
+
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+ public class MainApplication extends NavigationApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+      @Override
+      public boolean isDebug() {
+          return BuildConfig.DEBUG;
+      }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
+     protected List<ReactPackage> getPackages() {
+         // Add additional packages you require here
+         // No need to add RnnPackage and MainReactPackage
+         return Arrays.<ReactPackage>asList(
             new RNSpinkitPackage(),
             new RCTCameraPackage(),
-            new NavigationReactPackage(),
             new KCKeepAwakePackage(),
             new RandomBytesPackage(),
             new KeychainPackage()
-      );
-    }
-  };
+       );
+     }
+
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public List<ReactPackage> createAdditionalReactPackages() {
+      return this.getPackages();
   }
 }
